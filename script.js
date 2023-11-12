f01e007d807e7e3d8b9c7592a95a262c6371a215
KavinduSooriyaarachchi <kavindusooriyaarachchi4@gmail.com>
2023-11-12 11:08:56 +0530

    final commit

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const firstRandomNumber =
 const firstDiceImage = 'assets/dice' + firstRandomNumber + '.svg';
 
 
+function btn1click(){
 document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage);
 
+}
 
 // Generate a random number from 1-6
 
@@ -18,5 +20,17 @@ const secondRandomNumber =
 // Random dice image
 const secondDiceImage = 'assets/dice' + secondRandomNumber + '.svg';
 
-
+function btn2click(){
 document.querySelectorAll('img')[1].setAttribute('src',secondDiceImage);
+
+
+// logic for winner
+if(firstRandomNumber>secondRandomNumber){
+  document.querySelector('h1').innerHTML = 'The Winner is User 1';
+}else if(firstRandomNumber<secondRandomNumber){
+  document.querySelector('h1').innerHTML = 'The Winner is User 2';
+}else{
+  document.querySelector('h1').innerHTML = 'It is a Draw!';
+}
+}
+
