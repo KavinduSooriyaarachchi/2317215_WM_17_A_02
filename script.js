abe340e2aa15f0d7cf2ad2c0778f635568bf6d4c
KavinduSooriyaarachchi <kavindusooriyaarachchi4@gmail.com>
2023-11-12 08:13:06 +0530

    js part

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// Generate a random number from 1-6
+
+const firstRandomNumber =
+  Math.floor(Math.random()*6)+1
+
+// Random dice image
+const firstDiceImage = 'assets/dice' + firstRandomNumber + '.svg';
 
 
-let ss = "hello world" ;
-let a = ss.split(" ");
-console.log(a);
+document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage);
+
+
+// Generate a random number from 1-6
+
+const secondRandomNumber =
+  Math.floor(Math.random()*6)+1
+
+// Random dice image
+const secondDiceImage = 'assets/dice' + secondRandomNumber + '.svg';
+
+
+document.querySelectorAll('img')[1].setAttribute('src',secondDiceImage);
